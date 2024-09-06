a7797dd751c8a9450e7122fef4684776b91cc3c9
Moddex <152146781+ModdexEk@users.noreply.github.com>
2024-09-06 16:19:56 +0500

    1

--- src/components/yamap.jsx
+++ src/components/yamap.jsx
@@ -1,9 +1,11 @@
-import { YMaps, Map } from "@pbe/react-yandex-maps"
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps"
  export const Yamap = () => {
      return (
         <YMaps>
         <div className="contacts__map-block"> 
-          <Map defaultState={{ center: [55.75, 37.57], zoom: 12 }} />
+          <Map width='100%' height='100%'defaultState={{ center: [56.895555, 60.637315], zoom: 16 }}>
+          <Placemark defaultGeometry={[56.895555, 60.637315]} />
+          </Map>
         </div>
       </YMaps>
     )
